3aeace1e85e9c41bb4ce11f9e22c1fcf3a37c13e
HappyRehund <rayhanardian045@gmail.com>
2025-07-28 21:41:53 +0700

    fix structure (only notebook), and added multi-agents with reflection

--- src/chains/__init__.py
+++
@@ -1,3 +0,0 @@
-from .chains import guardrails_chain, reflection_chain, synthesis_chain, entity_chain
-
-__all__ = ['guardrails_chain', 'reflection_chain', 'synthesis_chain', 'entity_chain']
--- src/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .state import AgentState
-from .schemas import GuardrailsOutput, RephrasedQuestion, Entities
-
-__all__ = ['AgentState', 'GuardrailsOutput', 'RephrasedQuestion', 'Entities']
